b85bc6e5b606af4a3e3fd2f108dac4ebe1995d47
XardnoX <91823168+XardnoX@users.noreply.github.com>
2023-06-09 13:07:24 +0200

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost/knihy';
+    public $baseURL = https://xardnox.github.io/knihy';
 
     /**
      * --------------------------------------------------------------------------
